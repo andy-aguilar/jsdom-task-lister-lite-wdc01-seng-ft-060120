8013322d712544738eb4e5f7643dca0ee05af46a
andy-aguilar <aaguil3@gmail.com>
2020-07-15 17:43:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTask  = document.querySelector("[value='Create New Task']")
+  console.log(createTask)
+
+  createTask.addEventListener('click', function(e) {
+    e.preventDefault()
+    console.dir(e.target)
+
+    const liContent = document.getElementById("new-task-description").value 
+    // console.log(liContent)
+
+    const liElement = document.createElement("li")
+    const ulElement = document.getElementById("tasks")
+    liElement.innerText = liContent
+
+    let buttonTag = document.createElement("button")
+    // console.log(buttonTag)
+    // console.log(liElement)
+
+    buttonTag.innerText = "X"
+    liElement.appendChild(buttonTag)
+    ulElement.appendChild(liElement)
+
+    e.target.parentElement.reset()
+    
+    buttonTag.addEventListener('click', function(e){
+        e.target.parentElement.remove()
+    })
+
+  })
+
 });
